25ca49e34f7b7e51200aa4db945d2e4e6b3bdb2a
oladeleseyi <toothy61@gmail.com>
2021-11-26 00:26:41 +0100

    base form of query

--- resolvers/Query/index.js
+++ resolvers/Query/index.js
@@ -2,6 +2,9 @@ const Query = {
   hello: (_, { name }) => {
     return `Hello ${name}`;
   },
+  verifyAccountName: async (_, { bank_code, account_number, account_name }) => {
+    return account_name || "Place Holder";
+  },
 };
 
 export default Query;
--- schema.js
+++ schema.js
@@ -2,12 +2,16 @@ const schema = {
   schema: `
   type Query {
     hello(name: String!): String!
+    verifyAccountName(bank_code: String!, account_number: String!, account_name: String): String!
   }
   
   type Mutation {
-    verifyBankAccountNumber(user_account_number: String!, user_bank_code: String!, user_account_name: String!): String!
-  }  
-  `,
+    verifyBankAccountNumber(
+      user_account_number: String!
+      user_bank_code: String!
+      user_account_name: String!
+    ): String!
+  }`,
 };
 
 export default schema;
