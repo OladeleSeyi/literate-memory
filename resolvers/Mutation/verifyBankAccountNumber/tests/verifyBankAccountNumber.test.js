b7b69ad468c0bd84104fcd5ce8795fbb16dd6fe5
oladeleseyi <toothy61@gmail.com>
2021-11-26 16:55:39 +0100

    Naive test verifyBankAccount

---
+++ resolvers/Mutation/verifyBankAccountNumber/tests/verifyBankAccountNumber.test.js
@@ -0,0 +1,15 @@
+import verifyBankAccountNumber from "..";
+
+describe("Tests the VerifyBankAccountNumber Query resolver", () => {
+  test("should (naively) test the verifyBankAccountNumber query", async (done) => {
+    const vars = {
+      user_account_number: "0724407028",
+      user_bank_code: "044",
+      user_account_name: "OLUWASEYI TAMUNOMIEBI OLADELE",
+    };
+    const mock = await verifyBankAccountNumber("_", vars);
+
+    expect(mock).toBe("OLUWASEYI TAMUNOMIEBI OLADELE");
+    done();
+  });
+});
