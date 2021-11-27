7aedd2f116ecb3141ff5e5d91439e0eb90706790
oladeleseyi <toothy61@gmail.com>
2021-11-27 13:14:41 +0100

    Tests: verifyBankNumber  failures

--- resolvers/Mutation/verifyBankAccountNumber/tests/verifyBankAccountNumber.test.js
+++ resolvers/Mutation/verifyBankAccountNumber/tests/verifyBankAccountNumber.test.js
@@ -12,4 +12,28 @@ describe("Tests the VerifyBankAccountNumber Query resolver", () => {
     expect(mock).toBe("OLUWASEYI TAMUNOMIEBI OLADELE");
     done();
   });
+
+  test("should fail if fed an invalid account", async (done) => {
+    const vars = {
+      user_account_number: "0724407021",
+      user_bank_code: "058",
+      user_account_name: "OLUWASEYI TAMUNOMIEBI OLADELE",
+    };
+    await expect(verifyBankAccountNumber("_", vars)).rejects.toThrow(
+      "Invalid Information"
+    );
+    done();
+  }, 15000);
+
+  test("should fail if fed an an impropername", async (done) => {
+    const vars = {
+      user_account_number: "0724407028",
+      user_bank_code: "044",
+      user_account_name: "OLUWoSEYIN TAMUNOMIEBI OLADLE", // 3 editDistance
+    };
+    await expect(verifyBankAccountNumber("_", vars)).rejects.toThrow(
+      "The user is unverified"
+    );
+    done();
+  }, 15000);
 });
