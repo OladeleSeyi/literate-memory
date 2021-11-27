4a1e6bcd2a9210483570b5ffdcc6c46c73d20a83
oladeleseyi <toothy61@gmail.com>
2021-11-27 13:00:03 +0100

    verifyAccount Query Tests

---
+++ resolvers/Query/verifyAccountName/tests/verifyAccountName.test.js
@@ -0,0 +1,22 @@
+import verifyAccountName from "../index";
+
+describe("These test all the various cases of verifyAccountNmae query", () => {
+  test("should test verifyAccountName", async (done) => {
+    const vars = {
+      bank_code: "044",
+      account_number: "0724407028",
+    };
+    const mock = await verifyAccountName("_", vars);
+    expect(mock).toBe("Oluwaseyi Tamunomiebi Oladele");
+    done();
+  });
+
+  test("should faill when supplied with invalid vars", async (done) => {
+    const vars = {
+      bank_code: "058",
+      account_number: "0724407028",
+    };
+    await expect(verifyAccountName("_", vars)).rejects.toThrow();
+    done();
+  }, 15000);
+});
